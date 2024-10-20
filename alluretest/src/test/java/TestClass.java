21ab6c5c16eb4240bcf80876e11def2eeece57e9
Masoma123628 <masoma.egov@gmail.com>
2024-10-20 04:13:19 -0700

    Add files via upload

---
+++ alluretest/src/test/java/TestClass.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void myTest() {
+        // Your test logic here
+        System.out.println("Running my UI test!");
+    }
+}
